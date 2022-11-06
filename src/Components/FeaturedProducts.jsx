038a06ca9988c3b32be736ea839fed6ee6d9d4f1
ahamefula nzubechi <nzube1999@gmail.com>
2022-11-06 05:29:54 -0800

    changes made

--- src/Components/FeaturedProducts.jsx
+++ src/Components/FeaturedProducts.jsx
@@ -1,24 +1,32 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const FeaturedProducts = ({ items }) => {
   return (
-    <div className="section-center">
-      {items.map((menuitem) => {
-        const { id, title, img, desc, price } = menuitem;
-        return (
-          <article key={id} className="menu-item">
-            <img src={img} alt="img" className="photo" />
-            <div className="item-info">
-              <header>
-                <h4>{title}</h4>
-                <h4 className="price">${price}</h4>
-              </header>
-              <p className="item-text">{desc}</p>
-            </div>
-          </article>
-        );
-      })}
-    </div>
+    <>
+      <Helmet>
+        <title>Login</title>
+        <meta name="description" content="featured products" />
+        <link rel="canonical" href="/featured" />
+      </Helmet>
+      <div className="section-center">
+        {items.map((menuitem) => {
+          const { id, title, img, desc, price } = menuitem;
+          return (
+            <article key={id} className="menu-item">
+              <img src={img} alt="img" className="photo" />
+              <div className="item-info">
+                <header>
+                  <h4>{title}</h4>
+                  <h4 className="price">${price}</h4>
+                </header>
+                <p className="item-text">{desc}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
